f0540df9a254a97df0a8cc0d4e91368afb2a562f
Winnie K <52090385+khuuwinnie@users.noreply.github.com>
2019-08-14 14:38:46 -0400

    Update imageblur3.rb

--- imageblur3.rb
+++ imageblur3.rb
@@ -13,26 +13,24 @@ class Image
         b = Marshal.load ( Marshal.dump(@image) ) #creates new unique copy each time
         @image.each_with_index do |row, row_id|
             row.each_with_index do |pixel, col_id|
-        if b[row_id][col_id] == 1
-            @image[row_id + 1][col_id] = 1 unless [row_id] == @image.length - 1 #1 row down
-            @image[row_id - 1][col_id] = 1 unless [row_id] == 0 #1 row up
-            @image[row_id][col_id + 1] = 1 unless [col_id] == @image[row_id].length - 1 #1 column right
-            @image[row_id][col_id - 1] = 1 unless [col_id] == 0 #1 column left
+              if b[row_id][col_id] == 1
+                @image[row_id + 1][col_id] = 1 unless [row_id] == @image.length - 1 #1 row down
+                @image[row_id - 1][col_id] = 1 unless [row_id] == 0 #1 row up
+                @image[row_id][col_id + 1] = 1 unless [col_id] == @image[row_id].length - 1 #1 column right
+                @image[row_id][col_id - 1] = 1 unless [col_id] == 0 #1 column left
+              end 
+            end
         end
-        end
-        end
-     end
+    end
+  end
 
-    def output_image
-      @image.each do |image| 
+  def output_image
+    @image.each do |image| 
       print image.join 
       print "\n"
     end
-
-end
-     
-     
   end
+end
 
 image = Image.new([
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
